40225d487e5701bf24bc8a86c7f30a028f5c1b53
Jan Provaznik <jan.provaznik@gmail.com>
2015-04-14 08:05:22 -0400

    Scale out heat stack
    Adds a module which allows scaling nodes in a heat stack
    deployed using tuskar templates - typically Overcloud in TripleO.
    This module will be used from CLI/UI.
    
    Change-Id: I284cb72b15ffd92a9e50550d84374dfe9e5f94c0

---
+++ tripleo_common/scale.py
@@ -0,0 +1,38 @@
+# Copyright 2015 Red Hat, Inc.
+# All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import logging
+
+from tripleo_common import libutils
+from tuskarclient.common import utils as tuskarutils
+
+LOG = logging.getLogger(__name__)
+
+
+class ScaleManager(object):
+    def __init__(self, tuskarclient, heatclient, plan_id=None, stack_id=None):
+        self.tuskarclient = tuskarclient
+        self.heatclient = heatclient
+        self.stack_id = stack_id
+        self.plan = tuskarutils.find_resource(self.tuskarclient.plans, plan_id)
+
+    def scaleup(self, role, num):
+        LOG.debug('updating role %s count to %d', role, num)
+        self.plan = self.tuskarclient.plans.patch(
+            self.plan.uuid, [{'name': '{0}::count'.format(role),
+                              'value': num}])
+        params = libutils.heat_params_from_templates(
+            self.tuskarclient.plans.templates(self.plan.uuid))
+        self.heatclient.stacks.update(self.stack_id, **params)
